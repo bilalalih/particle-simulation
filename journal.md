987a1fdabd3a69d0056a826e2ea066660c5cafb6
Bilal Alih <bilalalih6@gmail.com>
2026-06-02 22:35:08 +0100

    Third Journal Entry!

--- journal.md
+++ journal.md
@@ -1,4 +1,24 @@
-## Building a Single Particle Simulation
+# Building a Single Particle Simulation
 
-# Step 1: Create a main.cpp file using """touch main.cpp"""
-# Step 2: Create a SDL3 Window 
+## Step 1: Create a main.cpp file using:
+- `touch main.cpp`
+
+## Step 2: Create a SDL3 Window:
+- We first create a function `bool init();` to initialize our SDL.
+```bash
+# SDL_Init() is used to initialize SDL, so we can start using it.
+SDL_Init(SDL_INIT_VIDEO); # is specifically used to initialize the video subsystem.
+```
+- This function `bool init();` initializes our SDL and creates a window.
+- This function `bool loadMedia();` loads images.
+- This function `void close();` cleans up the resourse used to load the window
+
+```bash
+cd build
+
+# Compile with this:
+g++ ../main.cpp -o app.exe $(pkg-config --cflags --libs sdl3)
+
+# Run:
+./app.exe
+```
--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <SDL3/SDL.h>
-#include <iostream>
+#include <string>
 
 /* Constants */
-// Screen dimension constants
-constexpr int kScreenWidth{ 640 };
-constexpr int kScreenHeight{ 800 };
+namespace screenConstants
+{
+    // Screen dimension constants
+    constexpr int kScreenWidth{ 640 };
+    constexpr int kScreenHeight{ 800 };
+};
 
 /* Function Prototypes */
 // Starts up SDL and creates window
@@ -17,43 +20,140 @@ bool loadMedia();
 void close();
 
 /* Global Variables */
-// The window we'll be rendering to
-SDL_Window* gWindow{nullptr};
+namespace global
+{
+    // The window we'll be rendering to
+    SDL_Window* gWindow{ nullptr };
 
-// The surface contained by the window
-SDL_Surface* gScreenSurface{nullptr};
+    // The surface contained by the window
+    SDL_Surface* gScreenSurface{ nullptr };
 
-// The image we will load and show on the screen
-SDL_Surface* gScene{ nullptr };
+    // The image we will load and show on the screen
+    SDL_Surface* gScene{ nullptr };
+}
 
 bool init()
 {
     // Initialization Flag
     bool success{ true };
+    
     if (SDL_Init(SDL_INIT_VIDEO) == false)
     {
         SDL_Log("SDL could not initialize! SDL Error: %s/n", SDL_GetError());
-        return -1;
+        success = false;
+    } else 
+    {
+        // Create Window
+        if (global::gWindow = SDL_CreateWindow(
+            "Particle SImulation: Hello SDL3", 
+            screenConstants::kScreenWidth, 
+            screenConstants::kScreenHeight,
+            0
+            ); 
+            global::gWindow == nullptr
+        ){
+            SDL_Log("Window Could Not Be Created! SDL Error: %s/n", SDL_GetError());
+            success = false;
+        } else
+        {
+            // Get Window Surface
+            global::gScreenSurface = SDL_GetWindowSurface(global::gWindow);
+        }
     }
 
+    SDL_Log("SDL Initialized Succesfully");
+    return success;
+}
+
+bool loadMedia()
+{
+    // File loading flag
+    bool success{ true };
+
+    // Load splash image
+    std::string imagePath{"image.png"};
+    if (global::gScene = SDL_LoadPNG( imagePath.c_str()); global::gScene == nullptr)
+    {
+        SDL_Log("Unable To Load Image %s! SDL error: %s/n ",imagePath.c_str(), SDL_GetError());
+        success = false;
+    }
+
+    return success;
+}
+
+void close()
+{
+    // Clean up Surface
+    SDL_DestroySurface( global::gScene );
+    global::gScene = nullptr;
+
+    // Destroy Window
+    SDL_DestroyWindow( global::gWindow ); 
+    global::gWindow = nullptr;
+    global::gScreenSurface = nullptr;
+
+    // Quit SDL subsystems
+    SDL_Delay(100);
+    SDL_Quit();
 }
 
 
 int main(int argc, char* argv[])
 {
+    // Final Exit Code
+    int exitCode{ 0 };
     
-    std::cout << "SDL initialized successfully!" << std::endl;
-    SDL_Window* window = SDL_CreateWindow(
-        "Particle Simulator",
-        800,
-        600,
-        0
-    );
+    // Initialize:
+    if (init() == false)
+    {
+        SDL_Log("Unable To Initialize Program!\n");
+        exitCode = 1;
+    }
+    else
+    {
+        // Load Media
+        if (loadMedia() == false)
+        {
+            SDL_Log("Unable To Load Media!\n");
+            exitCode = 2;
+        }
+        else
+        {
+            // The quit flag
+            bool quit{ false };
 
-    SDL_Delay(3000);
+            // The event data
+            SDL_Event(e);
+            SDL_zero(e);
+            // The main loop
+            while (quit == false)
+            {
+                // Get event data
+                while (SDL_PollEvent( &e ) == true)
+                {
+                    // if event is quit type
+                    if (e.type == SDL_EVENT_QUIT)
+                    {
+                        // End the main loop
+                        quit = true;
+                    }
+                }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+                // Fill the surface white
+                SDL_FillSurfaceRect(global::gScreenSurface, nullptr, SDL_MapSurfaceRGB(global::gScreenSurface, 0xFF, 0xFF, 0xFF));
 
-    return 0;
+                // Render image on screen
+                SDL_BlitSurface(global::gScene, nullptr, global::gScreenSurface, nullptr);
+
+                // Update the surface
+                SDL_UpdateWindowSurface( global::gWindow );
+                
+            }
+        }
+    }
+
+    // Clean up
+    close();
+
+    return exitCode;
 }
